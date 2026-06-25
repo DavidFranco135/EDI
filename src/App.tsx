cf80d780722b1fb808ffb7f84b011235a287d457
DavidFranco135 <davidbhmg147@gmail.com>
2026-06-25 18:32:16 -0300

    Add TabelaPrecos route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Clientes } from './pages/Clientes';
 import { DocumentManager } from './pages/DocumentManager';
 import { Relatorios } from './pages/Relatorios';
 import { Configuracoes } from './pages/Configuracoes';
+import { TabelaPrecos } from './pages/TabelaPrecos';
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
             <Route path="/romaneios/novo" element={<DocumentManager type="romaneio" />} />
             <Route path="/romaneios/:id" element={<DocumentManager type="romaneio" />} />
             <Route path="/relatorios" element={<Relatorios />} />
+            <Route path="/tabela-precos" element={<TabelaPrecos />} />
             <Route path="/configuracoes" element={<Configuracoes />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
